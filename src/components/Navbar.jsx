ab758a59dba4106e10f86afea3d7524f9233cc32
OdeliaFink <odelia.finkel@gmail.com>
2023-04-30 18:32:26 -0400

    refactor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CV from '../images/CV.pdf';
 
 const NavigationBar = () => {
+  const handleDownloadResume = () => {
+    // Replace the URL with the location of your resume file
+    const resumeUrl = CV;
+    window.open(resumeUrl, '_blank');
+  };
   return (
     <>
       {/* <div style={{ margin: '3rem 5rem' }}> */}
@@ -29,6 +35,20 @@ const NavigationBar = () => {
           <Link style={{ textDecoration: 'none' }} to="/contact">
             Contact
           </Link>
+
+          <button
+            onClick={handleDownloadResume}
+            style={{
+              marginInline: '2rem',
+              backgroundColor: 'black',
+              color: 'white',
+              padding: '1rem 2rem',
+              border: 'none',
+              borderRadius: '2rem',
+            }}
+          >
+            Resume
+          </button>
         </div>
       </nav>
       {/* </div> */}
