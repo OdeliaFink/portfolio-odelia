4deb42d08e8e4dd525d533d901062774b5e5b241
OdeliaFink <odelia.finkel@gmail.com>
2023-07-04 22:19:55 -0400

    refactor

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -26,10 +26,6 @@ const Home = () => {
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </Description>
-          <Description>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </Description>
           <Link to="/projects">
             <StyledButton>see projects</StyledButton>
           </Link>
